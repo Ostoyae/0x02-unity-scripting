2f59f57397c45fc09c8a667a8fc617d2b4eb61dc
Martin <msmith3dart@icloud.com>
2020-02-24 13:34:27 -0800

    add project files and update gitignore

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 _relativePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _relativePosition =  transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPosition = player.transform.position;
+
+        transform.position = playerPosition + _relativePosition;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1;
+    private Vector3 _direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    private void FixedUpdate()
+    {
+        _direction.Set(0, 0, 0);
+
+        if (UpdateDirection(ref _direction))
+            GetComponent<Rigidbody>().AddForce((_direction * speed));
+
+        
+        // Debug.Log(string.Format("Direction = {0}", _direction));
+    }
+
+    private static bool UpdateDirection(ref Vector3 dir)
+    {
+        if (!Input.anyKey) return false;
+        
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            dir.x += 1;
+        }
+
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir.x -= 1;
+        }
+
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            dir.z += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            dir.z -= 1;
+        }
+
+        return true;
+
+    }
+
+}
